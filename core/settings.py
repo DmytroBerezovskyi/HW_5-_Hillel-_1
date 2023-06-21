7393942854adae4c55498189309edfe3055403cc
DmytroBerezovskyi <berezovscky.dmitrij1@gmail.com>
2023-06-21 20:17:56 +0200

    ДЗ 7. queryset methods, managment commands

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
     "catalog",
 ]
 
@@ -126,3 +127,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# AUTH_USER_MODEL = 'accounts.User'
