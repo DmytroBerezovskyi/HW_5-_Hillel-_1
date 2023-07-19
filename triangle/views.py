d3635455db5ebc6b9c7bf5a587d5be837d60b2ae
DmytroBerezovskyi <berezovscky.dmitrij1@gmail.com>
2023-07-19 16:29:53 +0200

    Remake hw9 with use forms django

--- triangle/views.py
+++ triangle/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .forms import Triangls
 
 
 def triangle(request):
+    # if this is a POST request we need to process the form data
     if request.method == "POST":
-        fside = request.POST.get("fside")
-        sside = request.POST.get("sside")
-        if fside != "" and sside != "":
-            gip = round(((int(fside) ** 2) + (int(sside) ** 2)) ** 0.5, 3)
-            return render(request, "results.html", {"gip": gip})
-    return render(request, "triangle.html")
+        # create a form instance and populate it with data from the request:
+        form = Triangls(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            katet_1 = form.cleaned_data["katet_1"]
+            katet_2 = form.cleaned_data["katet_2"]
+            if katet_2 > 0 and katet_1 > 0:
+                gip = round((((katet_1) ** 2) + ((katet_2) ** 2)) ** 0.5, 3)
+                # process the data in form.cleaned_data as required
+                # ...
+                # redirect to a new URL:
+                return render(request, "form.html", {"form": form, "gip": gip})
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = Triangls()
+
+    return render(request, "form.html", {"form": form})
